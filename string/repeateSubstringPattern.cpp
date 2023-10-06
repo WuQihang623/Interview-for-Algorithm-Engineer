8cacd9a2f64154c5bd8d9772cae99107c4817dd1
wqh <270024269@qq.com>
2023-10-06 21:59:14 +0800

    leetcode test

--- string/repeateSubstringPattern.cpp
+++ string/repeateSubstringPattern.cpp
@@ -4,7 +4,7 @@ using namespace std;
 class Solution {
 public:
     bool repeatedSubstringPattern(string s) {
-        for (int size=1; size < (int)(s.size()/2); size++){
+        for (int size=1; size <= (int)(s.size()/2); size++){
             bool flag = true;
             if (s.size() % size == 0){
                 string substr = s.substr(0, size);
