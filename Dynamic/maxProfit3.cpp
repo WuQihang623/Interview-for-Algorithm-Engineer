f2866acd6f9d9df7efac02d089561ba4cb3be4c2
wqh <270024269@qq.com>
2024-01-26 15:36:45 +0800

    merge

--- Dynamic/maxProfit3.cpp
+++ Dynamic/maxProfit3.cpp
@@ -1,23 +1,4 @@
 # include<vector>
-<<<<<<< HEAD
-using namespace std;
-
-class Solution {
-public:
-    int maxProfit(vector<int>& prices) {
-        if (prices.size() == 0) return 0;
-        vector<vector<int>> dp(prices.size(), vector<int>(5, 0));
-        dp[0][1] = -prices[0];
-        dp[0][3] = -prices[0];
-        for (int i = 1; i < prices.size(); i++) {
-            dp[i][0] = dp[i - 1][0];
-            dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
-            dp[i][2] = max(dp[i - 1][2], dp[i - 1][1] + prices[i]);
-            dp[i][3] = max(dp[i - 1][3], dp[i - 1][2] - prices[i]);
-            dp[i][4] = max(dp[i - 1][4], dp[i - 1][3] + prices[i]);
-        }
-        return dp[prices.size() - 1][4];
-=======
 # include<algorithm>
 using namespace std;
 
@@ -39,6 +20,5 @@ public:
             dp[i][4] = max(dp[i-1][4], dp[i-1][3]+prices[i]);
         }
         return dp[prices.size()-1][4];
->>>>>>> 0868a2953d4e57c3d8920c13eab4eaeb443a2144
     }
 };
