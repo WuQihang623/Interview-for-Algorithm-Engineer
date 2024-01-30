2dfe3690cb4ab04c57c1c8665c53c93fcb6c3fd8
wqh <270024269@qq.com>
2024-01-30 21:46:55 +0800

    字符串相减

--- Dynamic/minDistance.cpp
+++ Dynamic/minDistance.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 // 两个字符串的相减操作
 
+// 方法1
 class Solution {
 public:
     int minDistance(string word1, string word2) {
@@ -17,5 +18,25 @@ public:
             }
         }
         return word1.size()-dp[word1.size()][word2.size()] + word2.size()-dp[word1.size()][word2.size()];
+    }
+};
+
+// 方法2
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        // dp[i][j]表示要删除的最小操作数
+        // 那么初始化时dp[i][0] = i, dp[0][j]=j
+        vector<vector<int>> dp(word1.size()+1, vector<int> (word2.size()+1, 0));
+        for (int i=1; i<=word1.size(); i++) dp[i][0] = i;
+        for (int j=1; j<=word2.size(); j++) dp[0][j] = j;
+
+        for (int i=1; i<=word1.size(); i++) {
+            for (int j=1; j<=word2.size(); j++) {
+                if (word1[i-1]==word2[j-1]) dp[i][j] = dp[i-1][j-1];
+                else dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + 1;
+            }
+        }    
+        return dp[word1.size()][word2.size()];
     }
 };
