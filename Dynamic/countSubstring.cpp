ee2fc77ac657bd08ae74e7f847955d98a21292c6
wqh <270024269@qq.com>
2024-02-02 21:00:03 +0800

    回文子串

--- Dynamic/countSubstring.cpp
+++ Dynamic/countSubstring.cpp
@@ -1,4 +1,5 @@
 # include<string>
+# include<vector>
 using namespace std;
 
 // 回文子串
@@ -20,6 +21,32 @@ public:
             num += 1;
             i--;
             j++;
+        }
+        return num;
+    }
+};
+
+// 动态规划方法解法
+class Solution {
+public:
+    int countSubstrings(string s) {
+        vector<vector<bool>> dp (s.size(), vector<bool> (s.size(), false));
+        int num = 0;
+        // 如果遍历顺序是i从0到n-1，那么递推公式为dp[i+1][j-1]
+        for (int i=s.size()-1; i>=0; i--) {
+            for (int j=i; j<s.size(); j++) {
+                if (s[i]==s[j]) {
+                    // 当i==j或者为aa,bb这种情况时
+                    if (j-i<=1) {
+                        num += 1;
+                        dp[i][j] = true;
+                    }
+                    else if (dp[i+1][j-1]) {
+                        num += 1;
+                        dp[i][j] = true;
+                    }
+                }
+            }
         }
         return num;
     }
