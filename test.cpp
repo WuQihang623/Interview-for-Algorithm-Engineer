e8c3912a23c1872f0b900c6dbcf868d91009c0aa
wqh <270024269@qq.com>
2024-04-13 23:42:04 +0800

    interview

--- test.cpp
+++ test.cpp
@@ -1,16 +1,11 @@
 #include<iostream>
+#include<unordered_map>
 using namespace std;
 
 int main() {
-    int n=10;
-    int count=0;
-    while(n>=3) {
-        int n1 = n/3*3;  // 喝的数量
-        count += n1;
-        cout << count << endl;
-        n = n - n1 +  n1 / 3;  // 换到酒的数量
-        
-    }
-    count += n;
-    cout << count << endl;
+    unordered_map<int, int> umap;
+    umap[0] = 1;
+    umap[0]--;
+    cout << umap.count(0) << endl;
+    return 0;
 }
